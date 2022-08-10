b86089aa9a054e9ffc93cbc0916965b864aa0711
Christian Koranteng <korantengchristian@gmail.com>
2022-08-10 23:13:09 +0000

    Update cmdexec

--- cmd_exec.c
+++ cmd_exec.c
@@ -195,12 +195,10 @@ return (1);
 }
 else
 {
-do
-{
+do {
 wpd = waitpid(pd, &state, WUNTRACED);
 } while (!WIFEXITED(state) && !WIFSIGNALED(state));
 }
-
 datash->status = state / 256;
 return (1);
 }
